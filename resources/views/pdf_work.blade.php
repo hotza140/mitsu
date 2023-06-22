47a85a0a4a815318c6c2de4b53fb541722c5bdee
Aoner14 <jaturongsp@gmail.com>
2023-06-22 10:34:44 +0700

    HOT WORK PDF

--- resources/views/pdf_work.blade.php
+++ resources/views/pdf_work.blade.php
@@ -10,14 +10,31 @@
 
         <!-- Styles -->
 
-        <style>
+<style>
         @font-face {
             font-family: 'THSarabunNew';
             font-style: normal;
             font-weight: normal;
             src: url("{{ public_path('fonts/THSarabunNew.ttf') }}") format('truetype');
         }
-
+        @font-face {
+            font-family: 'THSarabunNew';
+            font-style: normal;
+            font-weight: bold;
+            src: url("{{ public_path('fonts/THSarabunNew Bold.ttf') }}") format('truetype');
+        }
+        @font-face {
+            font-family: 'THSarabunNew';
+            font-style: italic;
+            font-weight: normal;
+            src: url("{{ public_path('fonts/THSarabunNew Italic.ttf') }}") format('truetype');
+        }
+        @font-face {
+            font-family: 'THSarabunNew';
+            font-style: italic;
+            font-weight: bold;
+            src: url("{{ public_path('fonts/THSarabunNew BoldItalic.ttf') }}") format('truetype');
+        }
         html, body {
             font-family: "THSarabunNew";
         }
