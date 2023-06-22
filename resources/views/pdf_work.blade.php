e8f60d9df6383298e4e19f641c890fb8c52b737c
Aoner14 <jaturongsp@gmail.com>
2023-06-22 10:30:30 +0700

    HOT PDF WORK

--- resources/views/pdf_work.blade.php
+++ resources/views/pdf_work.blade.php
@@ -1,26 +1,28 @@
 !DOCTYPE html>
 <html lang="TH">
     <head>
-    <META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         
 
         <title>PDF WORK</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
         <!-- Styles -->
+
         <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-        </style>
+        @font-face {
+            font-family: 'THSarabunNew';
+            font-style: normal;
+            font-weight: normal;
+            src: url("{{ public_path('fonts/THSarabunNew.ttf') }}") format('truetype');
+        }
+
+        html, body {
+            font-family: "THSarabunNew";
+        }
+    </style>
+
     </head>
     <body>
        <center>ไทย</center>
