28daf87fe602c5f1fd09f57c048e6893b4ea3f2d
Aoner14 <jaturongsp@gmail.com>
2023-06-27 09:29:01 +0700

    HOT ITEM POINT

--- resources/views/backend/item_point/index_wait.blade.php
+++ resources/views/backend/item_point/index_wait.blade.php
@@ -144,7 +144,8 @@ src="{{$picture}}" style="width:100px">
                                     </div>
                                     <div class="card-block">
                                         <div class="dt-responsive table-responsive">
-                                            <table id="simpletable" class="table table-striped table-bordered nowrap">
+                                            <!-- <table id="simpletable" class="table table-striped table-bordered nowrap"> -->
+                                            <table id="" class="table table-striped table-bordered nowrap">
                                                 <thead>
                                                     <tr>
 
@@ -245,7 +246,8 @@ src="{{$picture}}" style="width:100px">
                                     </div>
                                     <div class="card-block">
                                         <div class="dt-responsive table-responsive">
-                                            <table id="simpletable2" class="table table-striped table-bordered nowrap">
+                                            <!-- <table id="simpletable2" class="table table-striped table-bordered nowrap"> -->
+                                            <table id="" class="table table-striped table-bordered nowrap">
                                                 <thead>
                                                     <tr>
 
