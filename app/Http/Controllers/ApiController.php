97473bc5506d8c9f076a59039c332a061eb22520
Thanarat <thanarat.ke@gmail.com>
2023-07-04 09:21:03 +0700

    Update sender.

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -130,7 +130,7 @@ class ApiController extends Controller
                 CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
                 CURLOPT_CUSTOMREQUEST => 'POST',
                 CURLOPT_POSTFIELDS => '{
-            "sender": "Mitsu",
+            "sender": "MitsuHeavy",
             "msisdn": ["' . $phone . '"],
             "message": "รหัส OTP ของคุณคือ ' . $otp . ' รหัสอ้างอิง ' . $pass_check . ' รหัสมีอายุการใช้งาน 5 นาที ห้ามบอก OTP นี้แก่ผู้อื่นไม่ว่ากรณีใด"
         }',
