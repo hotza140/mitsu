4c680c7cc6bff0e51788972d562e8a8a66fbbfc3
Aoner14 <jaturongsp@gmail.com>
2023-07-10 11:19:13 +0700

    HOT AIR

--- resources/views/backend/wo/edit.blade.php
+++ resources/views/backend/wo/edit.blade.php
@@ -90,7 +90,7 @@
 
                                         <?php $aii=App\AirModel::orderby('id','desc')->get(); ?>
                                         <div class="form-group row">
-                                            <div class="col-sm-6">
+                                            <div class="col-sm-4">
                                                 <label class="col-form-label">Air Models</label>
                                                 <!-- <input type="text" name="air_model" class="form-control" id="pic"
                                                     value="<?php if(isset($item)){echo $item->air_model;} ?>"> -->
