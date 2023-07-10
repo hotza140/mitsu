b1a8594b5122d2f471d199a851062a328645ded0
Aoner14 <jaturongsp@gmail.com>
2023-07-10 11:09:47 +0700

    HOT AIR

--- app/AirModel.php
+++ app/AirModel.php
@@ -10,4 +10,6 @@ class AirModel extends Model
     use SoftDeletes;
     protected $table = "air_models";
     protected $primarykey = "id";
+
+    protected $fillable = ['model_name','des','id','point','number','created_at','updated_at'];
 }
