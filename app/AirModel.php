46d72aa378ab69e5c03cd2cca6bce1eeb8c6d08f
Aoner14 <jaturongsp@gmail.com>
2023-07-10 11:10:02 +0700

    HOT

--- app/AirModel.php
+++ app/AirModel.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class AirModel extends Model
 {
-    use SoftDeletes;
+    // use SoftDeletes;
     protected $table = "air_models";
     protected $primarykey = "id";
 
