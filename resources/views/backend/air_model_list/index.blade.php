7b3956a6193ec69e2a3515aeddd7facb7d1a66f1
Aoner14 <jaturongsp@gmail.com>
2023-07-24 10:48:08 +0700

    HOT

--- resources/views/backend/air_model_list/index.blade.php
+++ resources/views/backend/air_model_list/index.blade.php
@@ -27,14 +27,14 @@
                             <div class="card">
                                 <div class="card-header">
 
-                                      <form action="{{url('backend/air_model_list_excel')}}" method="POST" enctype="multipart/form-data">
+                                      <!-- <form action="{{url('backend/air_model_list_excel')}}" method="POST" enctype="multipart/form-data">
                                     @csrf
                                     <h6 style="color: red">(* อัพโหลดไฟล์ Excel ข้อมูล Model )</h6>
                                     <button type="submit" class="btn btn-danger" onclick="javascript:return confirm('Confirm?')" id="main"> Import </button>
                                     <input type="file" name="file"  />
                                    
                                     </form>
-                                    <br>
+                                    <br> -->
 
                                     <a style="color:white;" class="btn btn-success" href="{{url('/backend/air_model_list_add')}}">
                                         <i class="fa fa-plus"></i> Add</a>
