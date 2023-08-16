2b9e52c21454cdc9d081fa5239a8641e8bb2112b
Aoner14 <jaturongsp@gmail.com>
2023-08-16 11:40:52 +0700

    HOT FIZ

--- resources/views/backend/wo/add.blade.php
+++ resources/views/backend/wo/add.blade.php
@@ -87,7 +87,7 @@
                                         <div class="form-group row">
                                             <div class="col-sm-3">
                                                 <label class="col-form-label">Air Models</label>
-                                                <input type="text" name="air_model" class="form-control" id="pic"
+                                                <input type="text" name="model_name" class="form-control" id="pic"
                                                     value="">
                                             </div>
                                         </div>
