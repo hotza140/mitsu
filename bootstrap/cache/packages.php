c2dc93b8500c8367515eee697003fc11b83ca913
Aoner14 <jaturongsp@gmail.com>
2023-11-27 11:18:58 +0700

    HOT UPdate  yajra/laravel-datatables-oracle

--- bootstrap/cache/packages.php
+++ bootstrap/cache/packages.php
@@ -92,4 +92,15 @@
       'Alert' => 'UxWeb\\SweetAlert\\SweetAlert',
     ),
   ),
+  'yajra/laravel-datatables-oracle' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Yajra\\DataTables\\DataTablesServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'DataTables' => 'Yajra\\DataTables\\Facades\\DataTables',
+    ),
+  ),
 );
--- config/app.php
+++ config/app.php
@@ -163,6 +163,7 @@ return [
         Illuminate\View\ViewServiceProvider::class,
         Mccarlosen\LaravelMpdf\LaravelMpdfServiceProvider::class,
         Maatwebsite\Excel\ExcelServiceProvider::class,
+        Yajra\DataTables\DataTablesServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -230,6 +231,7 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'PDF' => Mccarlosen\LaravelMpdf\Facades\LaravelMpdf::class,
         'Excel' => Maatwebsite\Excel\Facades\Excel::class,
+        'DataTables' => Yajra\DataTables\Facades\DataTables::class,
 
     ],
 
