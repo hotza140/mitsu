d8b823344a87296c0f5a6e6fcd0ca0eeaa3597fd
Aoner14 <jaturongsp@gmail.com>
2024-01-23 17:25:44 +0700

    HOT FIX

--- resources/views/backend/user/index_item.blade.php
+++ resources/views/backend/user/index_item.blade.php
@@ -66,15 +66,19 @@
                                                         <td>{{($items->outdoor_number) ?? '-'}}</td>
                                                         <td>{{($items->in_name)?? '-'}}</td>
                                                         <td>{{($items->indoor_number)?? '-'}}</td>
-                                                        <!-- @if($items->created_at<'2024-01-23 10:40:01')
+                                                        @if($items->created_at>'2024-01-23 10:40:01' and $items->created_at<'2024-01-23 17:30:01')
                                                         <?php  
-                                                        $sum=$items->point+$items->point_2; 
+                                                        if($items->point>$items->point_2){
+                                                        $sum=$items->point+$items->point_2;
+                                                        }else{
+                                                        $sum=$items->point-$items->point_2;   
+                                                        }
                                                         ?>
                                                         <td>{{$sum}}</td>
                                                         @else
                                                         <td>{{($items->point)?? '-'}}</td>
-                                                        @endif -->
-                                                        <td>{{($items->point)?? '-'}}</td>
+                                                        @endif
+                                                        <!-- <td>{{($items->point)?? '-'}}</td> -->
                                                         <td>{{$items->created_at}}</td>
                                                        
                                                         <td>
