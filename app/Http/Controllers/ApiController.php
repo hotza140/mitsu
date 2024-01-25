65b45c880de8d2804cc5c99763442ec51797d836
Aoner14 <jaturongsp@gmail.com>
2024-01-25 10:20:16 +0700

    HOT FIX

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -89,7 +89,7 @@ class ApiController extends Controller
                 $user->bank_name=$r->bank_name;
                 }
 
-            if(@$r->bank_number!=null or @$r->bank_title!=null){
+            if(@$r->bank_number!=null or @$r->bank_title!=null or @$r->bank_name!=null){
             $user->save();
             }
 
