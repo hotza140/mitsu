ad8a6e3c3cd45f484c994953749d5a0dacc4c837
Aoner14 <jaturongsp@gmail.com>
2024-03-11 11:21:45 +0700

    HOt FIX

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -1721,12 +1721,18 @@ class ApiController extends Controller
         $date = date('Y-m-d');
         $date_a = date('Y-m-d', strtotime($date . ' + 7 days'));
         if ($r->date == null or $r->date == "null") {
-            $wo = WO::where('technician_id', '!=', null)->where('technician_id', $r->id)->wheredate('wo_date', '>=', $date)
-                ->wheredate('wo_date', '<=', $date_a)
+            $wo = WO::where('technician_id', '!=', null)->where('technician_id', $r->id)
+            ->whereBetween('wo_date', [$date, $date_a])
+            // ->wheredate('wo_date', '>=', $date)
+            //     ->wheredate('wo_date', '<=', $date_a)
                 ->where('d_status', 0)->with('customer')->with('model')->orderby('wo_time', 'asc')->get();
         } else {
-            $date = $r->date;
-            $wo = WO::where('technician_id', '!=', null)->where('technician_id', $r->id)->wheredate('wo_date', $date)->where('d_status', 0)->with('customer')->with('model')->orderby('wo_time', 'asc')->get();
+            $date_s = $r->date;
+            $date_sa = date('Y-m-d', strtotime($date_s . ' + 7 days'));
+            $wo = WO::where('technician_id', '!=', null)->where('technician_id', $r->id)
+            // ->wheredate('wo_date', $date)
+            ->whereBetween('wo_date', [$date_s, $date_sa])
+            ->where('d_status', 0)->with('customer')->with('model')->orderby('wo_time', 'asc')->get();
         }
 
         $message = "Success!";
