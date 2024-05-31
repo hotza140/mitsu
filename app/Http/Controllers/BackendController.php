b6f11df71dcede0c033dd469d53c9bba73ee20a7
Aoner14 <jaturongsp@gmail.com>
2024-05-31 10:44:17 +0700

    ็HOT FIX

--- app/Http/Controllers/BackendController.php
+++ app/Http/Controllers/BackendController.php
@@ -147,15 +147,15 @@ class BackendController extends Controller
 
 
         $bigQuery = new BigQueryClient();
-        $query = 'SELECT * FROM DWH_AC_SYNC_THIRDPARTY.ac_serial_no';
+        // $query = 'SELECT * FROM DWH_AC_SYNC_THIRDPARTY.ac_serial_no';
         // $query = BigQuery::query('SELECT * FROM DWH_AC_SYNC_THIRDPARTY.ac_serial_no');
-
+        $query = 'SELECT * FROM mahajak-data-warehouse.DWH_AC_SYNC_THIRDPTY.ac_serial_no';
+        
         $queryJobConfig = $bigQuery->query($query);
         $queryJobConfig->allowLargeResults(true);
         $queryJob = $bigQuery->startQuery($queryJobConfig);
         $queryJob->waitUntilComplete();
         $queryResults = $queryJob->queryResults();
-        dd($queryResults);
 
         foreach ($queryResults as $row) {
             print_r($row);
